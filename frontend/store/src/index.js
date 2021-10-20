f06b3d4b34945dadb6bd4ac8ca058279b018592d
vsterlikov <ipsat89@mail.ru>
2021-10-20 16:55:44 +0300

    добился вывода таблицы категорий

--- frontend/store/src/index.js
+++ frontend/store/src/index.js
@@ -1,47 +1,69 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-const client = require('rest');
 
+const rest = require('rest');
+const mime = require('rest/interceptor/mime');
+/*
+const defaultRequest = require('rest/interceptor/defaultRequest');
+const mime = require('rest/interceptor/mime');
+const uriTemplateInterceptor = require('./api/uriTemplateInterceptor');
+const errorCode = require('rest/interceptor/errorCode');
+const baseRegistry = require('rest/mime/registry');
+
+const registry = baseRegistry.child();
+*/
 class App extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {productCategories: []};
+		console.log("constructor");
+		this.state = {
+			productCategories: [],
+			testprop : "",
+		};
 	}
 
 	componentDidMount() {
-        /*
-        client({method: 'GET', path: '/api/productCategories'})
-		    .done(response => {this.setState({productCategories: response.entity._embedded.productCategories})
-                                }
-            );
-            */
+		/*
+		registry.register('text/uri-list', require('./api/uriListConverter'));
+		registry.register('application/hal+json', require('rest/mime/type/application/hal'));
 
-        client('/api/productCategories').then(response => {
-        	this.setState({productCategories : response.entity._embedded ? response.entity._embedded.productCategories : ""})
+		const client = rest
+			.wrap(mime, { registry: registry })
+			.wrap(uriTemplateInterceptor)
+			.wrap(errorCode)
+			.wrap(defaultRequest, { headers: { 'Accept': 'application/hal+json' }});
+        client({method: 'GET', path: '/api/productCategories'})
+		    .done(response => {
+		    	this.setState({productCategories: response.entity._embedded.productCategories})
+			});
+
+		*/
+		const client = rest.wrap(mime);
+        client({path: '/api/productCategories'}).then(response => {
+        	this.setState({productCategories : response.entity._embedded.productCategories});
         });
-                    /*
-		client('/api/productCategories')
-		    .then(response => { for (prop in response) {
+        /*
+    	client('/api/productCategories')
+		    .then(response => { for (prop in this.state.productCategories) {
 		                            console.log(`response[${prop}]=${response[prop]}`);
 		                        }
 		    });
-		    */
+		  */  
 
 	}
 
 	render() {
 		return (
-			/*<ProductCategoriesTable productCategories={this.state.productCategories}/>*/
-			<div>test2</div>
+			<ProductCategoriesTable productCategories={this.state.productCategories}/>
 		)
 	}
 }
 
 class ProductCategoriesTable extends React.Component{
 	render() {
-		const productCategories= this.props.productCategories
-		    .map(elem => <ProductCategory key={elem._links.self.href} productCategory={productCategory}/>);
+		const productCategories = this.props.productCategories
+		    .map(elem => <ProductCategory key={elem._links.self.href} productCategory={elem}/>);
 		return (
 			<table>
 				<tr>
